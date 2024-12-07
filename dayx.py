7a09a8c95ecd3173dad079c2963bfd6f2d0c19f6
Brian Lemke <brian.m.lemke@gmail.com>
2024-12-07 13:58:34 -0600

    Created Python template for a day and its test case to easily create new days

---
+++ dayx.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# https://adventofcode.com/2024/day/x
+
+import argparse
+import functools
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--inputfile', default='./inputs/2024_dayx.txt')
+
+def solve_a(inputfile):
+    """Read the file and return a solution"""
+    return None
+
+def solve_b(inputfile):
+    """Read the file and return a solution"""
+    return None
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    answer_a = solve_a(open(args.inputfile))
+    print(f'Answer (part A): {answer_a}')
+    answer_b = solve_b(open(args.inputfile))
+    print(f'Answer (part B): {answer_b}')
---
+++ dayx_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import unittest
+import dayx as solution
+
+class TestSolution(unittest.TestCase):
+    sample_input = """""".splitlines(keepends = True)
+
+    def test_sample_a(self):
+        """Make sure the sample solution works for part A"""
+        sample_solution = None
+
+        self.assertEqual(sample_solution, solution.solve_a(self.sample_input))
+
+    def test_sample_b(self):
+        """Make sure the sample solution works for part B"""
+        sample_solution = None
+
+        self.assertEqual(sample_solution, solution.solve_b(self.sample_input))
+
+if __name__ == '__main__':
+    unittest.main()
